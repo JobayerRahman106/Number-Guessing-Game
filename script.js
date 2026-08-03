b3a1d530c7a5bb86f3a52bdeb57b5ae78c8f62ba
Jobayer Rahman <jobayerrahman.diu@gmail.com>
2026-08-03 22:30:23 +0600

    Player1 and Player2 setup

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 let playeroneinput = document.querySelector(".playeroneinput")
 let playeronebutton = document.querySelector(".playeronebutton")
+let playertwoinput = document.querySelector(".playertwoinput")
+let playertwobutton = document.querySelector(".playertwobutton")
+let heading = document.querySelector(".heading")
 let error = document.querySelector(".error")
 
 playeronebutton.addEventListener("click", function(){
 
-    // console.log(typeof playeroneinput.value)
-    // console.log(Boolean(Number(playeroneinput.value)))
-    // console.log(isNaN(playeroneinput.value))
-
-   
-    if(playeroneinput.value == ""){
-        error.style.color = "red"
-        error.innerHTML = "Please give a number"
-    }
-    else{
-        if(Number(playeroneinput.value)){
-        error.innerHTML = ""
-    }
-    else{
-        error.style.color = "red"
-        error.innerHTML = "Please give a number"
-    }
+    if (playeroneinput.value == "") {
+      error.style.color = "red";
+      error.innerHTML = "Please give a number";
+    } 
+    else {
+      if (Number(playeroneinput.value)) {
+        error.innerHTML = "";
+        heading.innerHTML = "Player-2"
+        displayControl("p1np2b")
+      } else {
+        error.style.color = "red";
+        error.innerHTML = "Please give a number";
+      }
     }
 
-});
+})
+
+function displayControl (type){
+
+    if(type == "p1np2b"){
+        playeroneinput.style.display = "none"
+        playeronebutton.style.display = "none"
+        playertwoinput.style.display = "inline-block"
+        playertwobutton.style.display = "inline-block"
+    }
+}
